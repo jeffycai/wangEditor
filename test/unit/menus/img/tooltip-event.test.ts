a8db9c43fc2121182c3e018732f617deb0cb5917
luochao <651644100@qq.com>
2020-12-09 10:11:28 +0800

    test: 添加了paste-img 测试

---
+++ test/unit/menus/img/tooltip-event.test.ts
@@ -0,0 +1,10 @@
+/**
+ * @description Img menu tooltip-event
+ * @author luochao
+ */
+
+describe('Img menu tooltip-event', () => {
+    test('绑定 tooltip-event 事件', () => {
+        expect(true).toBeTruthy()
+    })
+})
